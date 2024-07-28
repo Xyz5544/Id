3144e10ebcadb1d32b80cd232efc2b95bd94a7a1
Xyz5544 <kumaridudu679@gmail.com>
2024-07-28 09:28:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", 27353035))
 API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
-OWNER_ID = int(getenv("OWNER_ID", "6752444356"))
+OWNER_ID = int(getenv("OWNER_ID", "7273427878"))
 STRING_SESSION = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
 SUPPORT_GRP = getenv("SUPPORT_GRP", "MEETUP_ZONE")
@@ -67,14 +67,14 @@ STICKER = [
 
 
 EMOJIOS = [
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
+    "👀",
+    "🥺",
+    "✨",
+    "🚩",
+    "🥵",
+    "😢",
+    "🌹",
+    "🌞",
+    "🥺",
+    "🫠",
 ]
